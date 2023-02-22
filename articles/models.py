3149e42a923df2590bb926c23128836e21328111
Michael Starr <starrtennis@gmail.com>
2023-02-21 18:47:21 -0800

    Done with tutorial

--- articles/models.py
+++ articles/models.py
@@ -1,13 +1,20 @@
 # articles/models.py
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.urls import reverse
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
+    slug = models.SlugField(null=False, unique=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("article_detail", args=[str(self.id)])
+        return reverse("article_detail", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
